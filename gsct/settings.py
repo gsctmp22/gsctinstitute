ad155dc2ba2e99e39dae354e262e374ca462063c
Ubuntu <ubuntu@ip-172-31-7-147.ap-south-1.compute.internal>
2020-12-03 09:38:58 +0000

    Server config

--- gsct/settings.py
+++ gsct/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -25,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'uukhlhwide6+izgmvo@p!h07r0vk#$bg7+yy+9-ln4#q6q$%d$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -131,5 +130,3 @@ LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
