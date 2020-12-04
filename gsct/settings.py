720ad4fdbb9e30379fec2953829b72c582a62e12
shekhar <shekharsharma8878@gmail.com>
2020-12-04 08:50:05 +0000

    Server config

--- gsct/settings.py
+++ gsct/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR, 'statics']
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'statics')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
